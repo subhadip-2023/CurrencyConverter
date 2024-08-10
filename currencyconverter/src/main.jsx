75930b7706ccaf599079f96e61c46abea009b13e
subhadip-2023 <subhadippal2k19@gmail.com>
2024-08-10 18:55:08 +0530

    Currency Converter

--- currencyconverter/src/main.jsx
+++ currencyconverter/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+const App = React.lazy(()=> App);
 createRoot(document.getElementById('root')).render(
     <App />
 )
